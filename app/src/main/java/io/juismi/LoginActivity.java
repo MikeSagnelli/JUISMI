cdceb6c56cb3cc37f1dd907ad82d2c0ab693ad48
Julia Orduño <juliapaola97@gmail.com>
2018-02-18 18:05:45 -0600

    Login layout design

--- app/src/main/java/io/juismi/LoginActivity.java
+++ app/src/main/java/io/juismi/LoginActivity.java
@@ -1,16 +1,13 @@
 package io.juismi;
 
-import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
-public class MainActivity extends AppCompatActivity {
+public class LoginActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
-
-        Intent intent = new Intent(this, LoginActivity.class);
+        setContentView(R.layout.activity_login);
     }
 }
