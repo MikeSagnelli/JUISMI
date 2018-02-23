8c2a20a9468add2a1c0204d796514fac39ee72c0
Mike Sagnelli <ing.sagnelli@gmail.com>
2018-02-22 23:45:58 -0600

    Solved merge conflicts

--- app/src/main/java/io/juismi/MainActivity.java
+++ app/src/main/java/io/juismi/MainActivity.java
@@ -4,14 +4,38 @@ import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+
 public class MainActivity extends AppCompatActivity {
+
+    private FirebaseAuth mAuth;
+
+    private static final int REGISTER_ACTIVITY = 0;
+    private static final int LOGIN_ACTIVITY = 1;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        this.mAuth = FirebaseAuth.getInstance();
+
+        boolean sessionExists = this.checkUser();
+    }
+
+    public boolean checkUser(){
+        FirebaseUser currentUser = this.mAuth.getCurrentUser();
+        return (currentUser != null);
+    }
+
+    public void requestLogin(){
         Intent intent = new Intent(this, LoginActivity.class);
-        startActivityForResult(intent, 0);
+        startActivityForResult(intent, LOGIN_ACTIVITY);
+    }
+
+    public void requestRegister(){
+        Intent intent = new Intent(this, RegisterActivity.class);
+        startActivityForResult(intent, REGISTER_ACTIVITY);
     }
 }
