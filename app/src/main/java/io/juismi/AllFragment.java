5e2cd8186f0db342d1d6f852c99d0cdf7cef8f20
Julia Orduño <juliapaola97@gmail.com>
2018-04-13 11:54:15 -0500

    show tags in issues list, first approach

--- app/src/main/java/io/juismi/AllFragment.java
+++ app/src/main/java/io/juismi/AllFragment.java
@@ -2,6 +2,7 @@ package io.juismi;
 
 import android.content.DialogInterface;
 import android.content.Intent;
+import android.graphics.Color;
 import android.support.design.widget.FloatingActionButton;
 import android.support.v4.app.Fragment;
 import android.os.Bundle;
@@ -12,6 +13,7 @@ import android.view.ViewGroup;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
+import android.widget.RelativeLayout;
 import android.widget.Spinner;
 import android.widget.TextView;
 
@@ -20,6 +22,8 @@ import com.google.firebase.auth.FirebaseUser;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 
+import java.util.List;
+
 
 /**
  * Created by JARVIS on 11/04/2018.
@@ -27,16 +31,17 @@ import com.google.firebase.database.FirebaseDatabase;
 
 public class AllFragment extends Fragment{
 
-    private ListView listView;
+    private ListView listView, tagsList;
     private FloatingActionButton addIssue;
     private Spinner filter;
 
     private FirebaseAuth mAuth;
     private FirebaseUser user;
     private DatabaseReference db;
-    private FirebaseAdapter adapter;
+    private FirebaseAdapter adapter, tagsAdapter, tagRowAdapter;
     private String boardID;
     private TagFilterDialog tfd;
+    private AdapterView.OnItemClickListener listener;
 
     private static final int REGISTER_ISSUE = 0;
     private static final int ISSUE_DETAILS = 1;
@@ -53,8 +58,19 @@ public class AllFragment extends Fragment{
         this.db = FirebaseDatabase.getInstance().getReference();
 
         this.listView = view.findViewById(R.id.issuesList);
+        this.tagsList = view.findViewById(R.id.tagsList);
         this.addIssue = getActivity().findViewById(R.id.addIssue);
         this.filter = (Spinner) view.findViewById(R.id.filter);
+        this.listener = new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> adapterView, View view, int i, long l) {
+                String key = adapter.getKey(i);
+                Intent intent = new Intent(getActivity(), IssueDetail.class);
+                intent.putExtra("issue_key", key);
+                intent.putExtra("board_key", boardID);
+                startActivityForResult(intent, ISSUE_DETAILS);
+            }
+        };
 
         String[] filters = new String[]{"Clear Filter","Filter by User","Filter by Tag",""};
         ArrayAdapter<String> filterAdapter = new ArrayAdapter<String>(getActivity(), android.R.layout.simple_spinner_dropdown_item, filters);
@@ -96,18 +112,6 @@ public class AllFragment extends Fragment{
         this.db.child("tags").orderByChild("boardID").equalTo(this.boardID);
         this.setAdapter("boardID", this.boardID);
 
-
-        this.listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
-            @Override
-            public void onItemClick(AdapterView<?> adapterView, View view, int i, long l) {
-                String key = adapter.getKey(i);
-                Intent intent = new Intent(getActivity(), IssueDetail.class);
-                intent.putExtra("issue_key", key);
-                intent.putExtra("board_key", boardID);
-                startActivityForResult(intent, ISSUE_DETAILS);
-            }
-        });
-
         return view;
     }
 
@@ -118,9 +122,26 @@ public class AllFragment extends Fragment{
                 ((TextView)v.findViewById(R.id.nameTask)).setText(model.getName());
                 ((TextView)v.findViewById(R.id.issueStatus)).setText("Status: " + model.getStatusId());
                 ((TextView)v.findViewById(R.id.storyPoints)).setText("Story Points: " + String.valueOf(model.getPoints()));
+
+                int index = getModels().indexOf(model);
+                String key = getKey(index);
+                ListView tagsList = (ListView) v.findViewById(R.id.tagsListView);
             }
         };
         this.listView.setAdapter(this.adapter);
+        this.listView.setOnItemClickListener(this.listener);
+
+        this.tagsAdapter = new FirebaseAdapter<IssueModel>(this.db.child("issues").orderByChild(key).equalTo(value), IssueModel.class,R.layout.issues_tag, getActivity()){
+            @Override
+            protected void populateView(View v, IssueModel model) {
+                int index = getModels().indexOf(model);
+                String key = getKey(index);
+                ListView tagsList = (ListView) v.findViewById(R.id.tagsListView);
+                fillTags(key, tagsList);
+            }
+        };
+        this.tagsList.setAdapter(this.tagsAdapter);
+        this.tagsList.setOnItemClickListener(this.listener);
     }
 
     private void filterByTag(String tagID){
@@ -133,5 +154,19 @@ public class AllFragment extends Fragment{
             }
         };
         this.listView.setAdapter(this.adapter);
+    }
+
+    private void fillTags(String issueID, ListView listView){
+        this.tagRowAdapter = new FirebaseAdapter<TagModel>(this.db.child("tags").orderByChild("issues/"+issueID).equalTo(true), TagModel.class,R.layout.detail_tag_row, getActivity()){
+
+            @Override
+            protected void populateView(View v, TagModel model) {
+                RelativeLayout layout = (RelativeLayout) v.findViewById(R.id.rowLayout);
+                int index = getModels().indexOf(model);
+                String hexColor = String.format("#%06X", (0xFFFFFF & model.getColor()));
+                layout.setBackgroundColor(Color.parseColor(hexColor));
+            }
+        };
+        listView.setAdapter(this.tagRowAdapter);
     }
 }
