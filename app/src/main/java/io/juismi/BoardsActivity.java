99bcd67657adb9c931da0b82767ea7bfc8615dab
Mike Sagnelli <ing.sagnelli@gmail.com>
2018-04-13 04:48:00 -0500

    Fixed boards

--- app/src/main/java/io/juismi/BoardsActivity.java
+++ app/src/main/java/io/juismi/BoardsActivity.java
@@ -6,6 +6,7 @@ import android.support.design.widget.FloatingActionButton;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.ListView;
@@ -34,6 +35,9 @@ public class BoardsActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_boards);
+        getSupportActionBar().setTitle("BOARDS");
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+        getSupportActionBar().setDisplayShowHomeEnabled(true);
 
         this.listView = (ListView)findViewById(R.id.listView);
         this.addBoard = (FloatingActionButton)findViewById(R.id.addBoard);
@@ -67,6 +71,15 @@ public class BoardsActivity extends AppCompatActivity {
                 startActivityForResult(intent, ISSUES);
             }
         });
+    }
+
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // handle arrow click here
+        if (item.getItemId() == android.R.id.home) {
+            finish(); // close this activity and return to preview activity (if there is any)
+        }
+
+        return super.onOptionsItemSelected(item);
     }
 
     protected void onActivityResult(int requestCode, int resultCode, Intent data) {
